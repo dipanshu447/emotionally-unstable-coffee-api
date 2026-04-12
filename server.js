7686c628dfb9897c83173dea2842944834cdb6b2
dipanshu447 <dipanshusahu447@gmail.com>
2026-04-12 15:54:10 +0530

    final change for deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from './src/app.js';
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log("Coffee machine is unstable on port " + PORT + " ☕");
